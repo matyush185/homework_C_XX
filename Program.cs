18c214a64fc5ec851c8cf8238ebb7c6c9f48abe3
matyush185 <dmitry.matyukov@gmail.com>
2023-06-19 03:09:20 +0300

    feat 1..

--- Program.cs
+++ Program.cs
@@ -42,6 +42,7 @@ int SumNum (int min, int max)
 // if (num1 <= num2) Console.WriteLine($"Sum is {SumNum(num1,num2)}");
 // else Console.WriteLine($"Sum is {SumNum(num2,num1)}");
 
+
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
